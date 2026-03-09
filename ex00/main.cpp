448415a9815b40089c9729e59047e5cdb8292e2e
kaan-erk <kaanerktoraman@gmail.com>
2026-03-09 16:14:04 +0300

    Update

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,15 +1,34 @@
 #include "Fixed.hpp"
 
-int main(void) {
-    Fixed a;
-    Fixed b(a);
-    Fixed c;
+#define GREEN  "\033[1;32m"
+#define CYAN   "\033[1;36m"
+#define RESET  "\033[0m"
+
+int main(void)
+{
+    std::cout << GREEN;
+    std::cout << "  ______  __  ___          ___     ___  " << std::endl;
+    std::cout << " /  ____| \\ \\/  /         / _ \\   / _ \\ " << std::endl;
+    std::cout << "|  |__     \\  /   ______ | | | | | | | |" << std::endl;
+    std::cout << "|   __|    /  \\  |______|| | | | | | | |" << std::endl;
+    std::cout << "|  |____  / /\\  \\        | |_| | | |_| |" << std::endl;
+    std::cout << " \\______|/_/  \\__\\        \\___/   \\___/ " << std::endl;
+    std::cout << RESET << std::endl;
+
+    std::cout << CYAN << "\nCONSTRUCTORS CALLED" << RESET << std::endl;
+
+    Fixed   a;
+    Fixed   b(a);
+    Fixed   c;
+
+    std::cout << CYAN << "\nCOPY ASSINGMENT OPERATOR CALLED" << RESET << std::endl;
 
     c = b;
 
+    std::cout << CYAN << "\nMEMBER FUNCTIONS CALLED" << RESET << std::endl;
     std::cout << a.getRawBits() << std::endl;
     std::cout << b.getRawBits() << std::endl;
     std::cout << c.getRawBits() << std::endl;
 
-    return 0;
+    return (0);
 }
